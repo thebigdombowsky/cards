39c39384f36996075e31bf1404d5e9157bb4f6fe
Shane <sdombowsky@vertafore.com>
2019-12-09 07:10:23 -0700

    Add tests

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 
@@ -20,5 +23,17 @@ func TestNewDeck(t *testing.T) {
 }
 
 func TestSaveToDeckAndNewTestFromFile(t *testing.T) {
+
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
 
 }
